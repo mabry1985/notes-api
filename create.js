f1cbb1a8ed72bfadbbcd0df77b41e313df7c7c6a
Josh Mabry <mabry1985@gmail.com>
2020-11-17 11:46:59 -0800

    refactor

--- create.js
+++ create.js
@@ -1,31 +1,21 @@
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
-AWS.config.update({ region: "us-west-2" });
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-export async function main(event, context) {
+export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
     Item: {
       userId: "123",
       noteId: uuid.v1(),
-      content: data.content,
+      content: data.content, 
       attachment: data.attachment,
       createdAt: Date.now(),
     },
   };
 
-  try {
-    await dynamoDb.put(params).promise();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message }),
-    };
-  }
-}
+  await dynamoDb.put(params);
+
+  return params.Item;
+});
